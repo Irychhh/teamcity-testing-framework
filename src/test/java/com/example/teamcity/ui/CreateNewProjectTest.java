c34e5cdd49b7d7cad3b17ad50651be6d3869858f
Irina Shelter <i.shelter@rabota.ru>
2023-12-16 00:52:15 +0300

    2.6

---
+++ src/test/java/com/example/teamcity/ui/CreateNewProjectTest.java
@@ -0,0 +1,19 @@
+package com.example.teamcity.ui;
+
+import com.example.teamcity.ui.pages.admin.CreateNewProject;
+import org.testng.annotations.Test;
+
+public class CreateNewProjectTest extends BaseUiTest{
+    @Test
+    public void authorizedUserShouldBeAbleCreateNewProject() {
+        var testData = testDataStorage.addTestData();
+        var url = "https://github.com/Irychhh/team-city-workshop";
+
+        loginAsUser(testData.getUser());
+
+        new CreateNewProject()
+                .open(testData.getProject().getParentProject().getLocator())
+                .createProjectByUrl(url)
+                .setupProject(testData.getProject().getName(), testData.getBuildType().getName());
+    }
+}
